b7c9f0104db2bf8cb5f82c4af9639d59bbdce296
kadaradam <xadam1996@gmail.com>
2022-08-19 11:55:55 +0200

    feat: add user simple CRUD

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	constructor(private readonly usersService: UsersService) {}
+
+	@Get()
+	async get() {
+		return this.usersService.findAll();
+	}
+
+	@Get(':id')
+	async find(@Param('id') id: string) {
+		return this.usersService.findOne(id);
+	}
+
+	@Post()
+	async create(@Body() createUserDto: CreateUserDto) {
+		return this.usersService.create(createUserDto);
+	}
+
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+		return this.usersService.update(id, updateUserDto);
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return this.usersService.delete(id);
+	}
+}
--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto, UpdateUserDto } from './dto';
+import { CleanUser, User, UserDocument } from './user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+	async create(createUserDto: CreateUserDto): Promise<CleanUser> {
+		const userExists = await this.userModel.findOne({ username: createUserDto.username });
+
+		if (userExists) {
+			throw new HttpException('User already exists', HttpStatus.CONFLICT);
+		}
+
+		const createdUser = new this.userModel({
+			...createUserDto,
+		});
+
+		return createdUser.save();
+	}
+
+	async findAll(): Promise<CleanUser[]> {
+		return this.userModel.find().exec();
+	}
+
+	async findOne(id: string): Promise<CleanUser> {
+		return this.userModel.findById(id).exec();
+	}
+
+	async update(id: string, updateUserDto: UpdateUserDto): Promise<CleanUser> {
+		return this.userModel.findByIdAndUpdate(id, updateUserDto).exec();
+	}
+
+	async delete(id: string): Promise<CleanUser> {
+		return this.userModel.findByIdAndDelete(id).exec();
+	}
+}
