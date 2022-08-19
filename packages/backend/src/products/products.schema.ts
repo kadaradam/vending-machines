afb6b6da7965633973d0d1cebfd1e119a122b7f9
kadaradam <xadam1996@gmail.com>
2022-08-19 11:39:34 +0200

    feat: add products schema

---
+++ packages/backend/src/products/products.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { User } from 'src/users/user.schema';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+	@Prop({ required: true, type: MongooseSchema.Types.ObjectId, ref: User.name })
+	sellerId: User;
+
+	@Prop({ required: true })
+	productName: string;
+
+	@Prop()
+	amountAvailable: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
