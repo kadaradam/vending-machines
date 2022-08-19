7b1c93fd61c648810126b09b6754c812b6b809a2
kadaradam <xadam1996@gmail.com>
2022-08-19 16:31:03 +0200

    feat: add products controller

--- packages/backend/src/products/products.controller.ts
+++ packages/backend/src/products/products.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Post,
+	Put,
+	Request,
+	UseGuards,
+} from '@nestjs/common';
+import { SellerGuard } from 'src/auth/seller.guard';
+import { UserRequestType } from 'src/types';
+import { CreateProductDto, UpdateProductDto } from './dto';
+import { ProductsService } from './products.service';
 
+@UseGuards(SellerGuard)
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+	constructor(private readonly productsService: ProductsService) {}
+
+	@Get()
+	async get(@Request() req: UserRequestType) {
+		return this.productsService.findAll(req.user);
+	}
+
+	@Get(':id')
+	async find(@Request() req: UserRequestType, @Param('id') id: string) {
+		return this.productsService.findOne(req.user, id);
+	}
+
+	@Post()
+	async create(@Request() req: UserRequestType, @Body() createProductDto: CreateProductDto) {
+		return this.productsService.create(req.user, createProductDto);
+	}
+
+	@Put(':id')
+	async update(
+		@Request() req: UserRequestType,
+		@Param('id') id: string,
+		@Body() updateProductDto: UpdateProductDto,
+	) {
+		return this.productsService.update(req.user, id, updateProductDto);
+	}
+
+	@Delete(':id')
+	async delete(@Request() req: UserRequestType, @Param('id') id: string) {
+		return this.productsService.delete(req.user, id);
+	}
+}
