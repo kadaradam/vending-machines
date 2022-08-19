403902cf4c96d65845e31674d8e82bd1e9f7ae45
kadaradam <xadam1996@gmail.com>
2022-08-19 10:25:04 +0200

    feat: add backend / products module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
