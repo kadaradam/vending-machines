579fd63b6a7fca041d15dc00364dec3dad64014e
kadaradam <xadam1996@gmail.com>
2022-08-19 12:02:59 +0200

    feat: user.module add password hashing

--- packages/backend/src/users/users.module.ts
+++ packages/backend/src/users/users.module.ts
@@ -1,7 +1,38 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Document } from 'mongoose';
+import { User, UserSchema } from './user.schema';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
+	imports: [
+		MongooseModule.forFeatureAsync([
+			{
+				name: User.name,
+				useFactory: () => {
+					const schema = UserSchema;
+					schema.pre<User & Document>('save', async function (next) {
+						// eslint-disable-next-line @typescript-eslint/no-this-alias
+						const user = this;
+
+						// only hash the password if it has been modified (or is new)
+						if (!user.isModified('password')) return next();
+
+						const hashedPassword = await bcrypt.hash(user.password, 8);
+
+						user.password = hashedPassword;
+
+						next();
+					});
+
+					return schema;
+				},
+			},
+		]),
+	],
+	providers: [UsersService],
 	controllers: [UsersController],
 })
 export class UsersModule {}
