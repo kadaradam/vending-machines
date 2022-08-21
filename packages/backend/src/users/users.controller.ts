9d3e07372f92f9f9bac6a35cab475b7c31b4b812
kadaradam <xadam1996@gmail.com>
2022-08-21 16:40:24 +0200

    feat: users: add role guard

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Delete, Get, Post, Put, Request } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { RegisterDto } from 'src/auth/dto';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { UserRequestType } from 'src/types';
 import { UpdateUserDto } from './dto';
 import { DepositAmountDto } from './dto/deposit-amount.dto';
+import { RolesEnum } from './user.schema';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -13,7 +16,21 @@ export class UsersController {
 		private readonly authService: AuthService,
 	) {}
 
-	// Same as /auth/register route
+	@UseGuards(RolesGuard)
+	@Roles(RolesEnum.BUYER)
+	@Put('/deposit')
+	async deposit(@Request() req: UserRequestType, @Body() depositAmountDto: DepositAmountDto) {
+		return this.usersService.depositAmount(req.user, depositAmountDto);
+	}
+
+	@UseGuards(RolesGuard)
+	@Roles(RolesEnum.BUYER)
+	@Post('/reset')
+	async reset(@Request() req: UserRequestType) {
+		return this.usersService.resetDeposit(req.user);
+	}
+
+	// Alias: Same as /auth/register route
 	@Post()
 	async create(@Body() registerUserDto: RegisterDto) {
 		return this.authService.register(registerUserDto);
@@ -33,18 +50,4 @@ export class UsersController {
 	async delete(@Request() req: UserRequestType) {
 		return this.usersService.delete(req.user);
 	}
-
-	@Put('/deposit')
-	async deposit(@Request() req: UserRequestType, @Body() depositAmountDto: DepositAmountDto) {
-		// TODO: add check: only buyer
-		return this.usersService.depositAmount(req.user, depositAmountDto);
-	}
-
-	@Post('/reset')
-	async reset(@Request() req: UserRequestType) {
-		// TODO: add check: only buyer
-		return this.usersService.resetDeposit(req.user);
-	}
-
-	// Admin functions
 }
