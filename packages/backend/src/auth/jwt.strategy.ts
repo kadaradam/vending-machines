52e9e0c080f23458003cf8e466ff738eae3fe51a
kadaradam <xadam1996@gmail.com>
2022-08-21 14:46:40 +0200

    fix: jwt.strategy add user

--- packages/backend/src/auth/jwt.strategy.ts
+++ packages/backend/src/auth/jwt.strategy.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { CleanUser, User, UserDocument } from 'src/users/user.schema';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor() {
+	constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
@@ -13,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 		});
 	}
 
-	async validate(payload: any) {
-		return { userId: payload.sub, username: payload.username };
+	async validate(payload: any): Promise<CleanUser> {
+		return this.userModel.findById(payload.sub).exec();
 	}
 }
