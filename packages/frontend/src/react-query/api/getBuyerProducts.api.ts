942622f116a50f57f818923f7fc901a77d4ed066
kadaradam <xadam1996@gmail.com>
2022-08-25 18:55:02 +0200

    feat: Add getbuyerProducts api

---
+++ packages/frontend/src/react-query/api/getBuyerProducts.api.ts
@@ -0,0 +1,12 @@
+import { ProductType } from "src/types";
+import axiosService from "../../axiosService";
+
+export type getBuyerProductsResponse = ProductType[];
+
+export async function getBuyerProducts(): Promise<getBuyerProductsResponse> {
+  const { data } = await axiosService.instance.get<getBuyerProductsResponse>(
+    "/products"
+  );
+
+  return data;
+}
--- packages/frontend/src/react-query/api/index.ts
+++ packages/frontend/src/react-query/api/index.ts
@@ -1,4 +1,5 @@
 export * from "./depositBalance.api";
+export * from "./getBuyerProducts.api";
 export * from "./getMyUser.api";
 export * from "./login.api";
 export * from "./resetBalance.api";
