e66fffe024d507a4f0a0553c311bcdedeacd5bd4
kadaradam <xadam1996@gmail.com>
2022-08-25 18:54:29 +0200

    feat: F: add ProductItem type

---
+++ packages/frontend/src/components/ProductItem.tsx
@@ -0,0 +1,39 @@
+import { Box, Typography } from "@mui/material";
+import { ProductType } from "src/types";
+import { prettyCurrency } from "src/utils";
+
+type ProductItemProps = {
+  item: ProductType;
+  selected: boolean;
+  onClick: () => void;
+};
+
+export const ProductItem = ({ item, selected, onClick }: ProductItemProps) => {
+  const { productName, cost } = item;
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        padding: 1,
+        borderStyle: "solid",
+        display: "inline-block",
+        cursor: "pointer",
+        ...(selected
+          ? { borderWidth: 2, borderColor: "primary.main" }
+          : { borderWidth: 1, borderColor: "black" }),
+      }}
+      onClick={onClick}
+    >
+      <Typography variant="body2" sx={{ textAlign: "center" }}>
+        {productName}
+      </Typography>
+      <Typography
+        variant="caption"
+        sx={{ color: "green", textAlign: "center" }}
+      >
+        {cost ? prettyCurrency(cost) : 0}
+      </Typography>
+    </Box>
+  );
+};
--- packages/frontend/src/components/index.ts
+++ packages/frontend/src/components/index.ts
@@ -1,1 +1,2 @@
-export { default } from "./MainContainer";
+export * from "./MainContainer";
+export * from "./ProductItem";
