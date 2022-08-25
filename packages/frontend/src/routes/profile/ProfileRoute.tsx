3cde27aec08c778796da21f67a5bebc81e0c4a9b
kadaradam <xadam1996@gmail.com>
2022-08-25 21:39:41 +0200

    feat: add Profile route

---
+++ packages/frontend/src/routes/profile/ProfileRoute.tsx
@@ -0,0 +1,88 @@
+import { Box, Button, Card, Typography } from "@mui/material";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { MainContainer } from "src/components";
+import {
+  depositBalance,
+  getMyUserApi,
+  resetBalance,
+} from "src/react-query/api";
+import { Wallet } from "src/utils";
+
+const CardWithTitle = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => (
+  <Card sx={{ p: 2 }}>
+    <Typography variant="body2" gutterBottom sx={{ fontWeight: "bold" }}>
+      {title}
+    </Typography>
+    <Box>{children}</Box>
+  </Card>
+);
+
+const ProfileRoute = () => {
+  const { data: user, isLoading } = useQuery(["user"], getMyUserApi);
+  const { mutate: reset, isLoading: isResetLoading } = useMutation(
+    resetBalance,
+    {
+      onSuccess: () => toast("Success"),
+    }
+  );
+  const { mutate: deposit, isLoading: isDepositLoading } = useMutation(
+    depositBalance,
+    {
+      onSuccess: () => toast("Success"),
+    }
+  );
+
+  console.log(user);
+
+  // TODO: Add spinner
+  if (isLoading || !user) {
+    return null;
+  }
+
+  return (
+    <MainContainer sx={{ pt: 2 }}>
+      <CardWithTitle title={`${user.username}'s wallet`}>
+        <Box
+          width="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography variant="body2">
+            Balance: {Wallet(user.deposit).getBalance()}
+          </Typography>
+          <Box>
+            <Button
+              onClick={() =>
+                deposit({ coins: { 100: 1, 50: 0, 20: 0, 10: 0, 5: 0 } })
+              }
+              disabled={isDepositLoading}
+              variant="contained"
+              color="primary"
+              sx={{ mr: 1 }}
+            >
+              Add Balance
+            </Button>
+            <Button
+              onClick={() => reset()}
+              disabled={isResetLoading}
+              variant="contained"
+              color="secondary"
+            >
+              Reset Balance
+            </Button>
+          </Box>
+        </Box>
+      </CardWithTitle>
+    </MainContainer>
+  );
+};
+
+export default ProfileRoute;
--- packages/frontend/src/routes/routes.tsx
+++ packages/frontend/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import PageNotFoundRoute from "./404";
 import DashboardRoute from "./dashboard";
 import LoginRoute from "./login";
+import ProfileRoute from "./profile";
 import RegisterRoute from "./register";
 import RequireAuth from "./RequireAuth";
 import RequireUnAuth from "./RequireUnAuth";
@@ -12,6 +13,15 @@ const routes = [
     element: (
       <RequireAuth>
         <DashboardRoute />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: "/profile",
+    exact: true,
+    element: (
+      <RequireAuth>
+        <ProfileRoute />
       </RequireAuth>
     ),
   },
