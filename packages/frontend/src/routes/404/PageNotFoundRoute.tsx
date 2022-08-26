b6b1706706947f393763b99d70115bb4763a8911
kadaradam <xadam1996@gmail.com>
2022-08-26 09:16:36 +0200

    feat: add 404 page

--- packages/frontend/src/routes/404/PageNotFoundRoute.tsx
+++ packages/frontend/src/routes/404/PageNotFoundRoute.tsx
@@ -1,5 +1,25 @@
+import { Box, Button, Typography } from "@mui/material";
+
 const PageNotFoundRoute = () => {
-  return <div></div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h6">
+        The page you’re looking for doesn’t exist.
+      </Typography>
+      <Button href="/" variant="outlined" sx={{ mt: 3 }}>
+        Back Home
+      </Button>
+    </Box>
+  );
 };
 
 export default PageNotFoundRoute;
