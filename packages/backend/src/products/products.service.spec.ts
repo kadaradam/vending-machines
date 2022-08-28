45ead410efeac7eaa383cb47d3a68d9d7317f04c
kadaradam <xadam1996@gmail.com>
2022-08-28 23:30:03 +0200

    feat: add product create test

--- packages/backend/src/products/products.service.spec.ts
+++ packages/backend/src/products/products.service.spec.ts
@@ -1,10 +1,35 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { RolesEnum } from '@vending/types/src';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { connect, Connection, Model } from 'mongoose';
-import { User, UserSchema } from 'src/users/user.schema';
+import mongoose, { connect, Connection, Model } from 'mongoose';
+import { CleanUser, User, UserSchema } from 'src/users/user.schema';
 import { Product, ProductSchema } from './products.schema';
 import { ProductsService } from './products.service';
+
+const CleanUserDTOStub = (): CleanUser => {
+	return {
+		session: '-',
+		_id: new mongoose.Types.ObjectId('630bd1f86d320d062f172244'),
+		deposit: { 100: 0, 50: 0, 20: 0, 10: 0, 5: 0 },
+		role: RolesEnum.BUYER,
+		username: 'John Doe',
+	};
+};
+
+const CreateProductDTOStub = (): Pick<Product, 'productName' | 'cost'> => {
+	return {
+		cost: 200,
+		productName: 'Cola',
+	};
+};
+
+class ProductAlreadyExists extends HttpException {
+	constructor() {
+		super('Product already exists', HttpStatus.CONFLICT);
+	}
+}
 
 describe('ProductsService', () => {
 	let service: ProductsService;
@@ -53,5 +78,26 @@ describe('ProductsService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	describe('addProduct', () => {
+		it('should create new product', async () => {
+			const withUser = CleanUserDTOStub();
+			const toCreateProduct = CreateProductDTOStub();
+			const product = await service.create(withUser, toCreateProduct);
+
+			expect(product.productName).toBe(toCreateProduct.productName);
+			expect(product.cost).toBe(toCreateProduct.cost);
+			expect(product.sellerId).toBe(withUser._id);
+		});
+
+		it('should create new product and throw exist error', async () => {
+			const withUser = CleanUserDTOStub();
+			const toCreateProduct = CreateProductDTOStub();
+
+			await expect(service.create(withUser, toCreateProduct)).rejects.toThrow(
+				new ProductAlreadyExists(),
+			);
+		});
 	});
 });
