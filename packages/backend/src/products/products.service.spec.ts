fdee168b9e39b9460fc956521963d122c59d7741
kadaradam <xadam1996@gmail.com>
2022-08-28 23:01:14 +0200

    fix: product.service.spec: add mongoose to module

--- packages/backend/src/products/products.service.spec.ts
+++ packages/backend/src/products/products.service.spec.ts
@@ -1,15 +1,54 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { connect, Connection, Model } from 'mongoose';
+import { User, UserSchema } from 'src/users/user.schema';
+import { Product, ProductSchema } from './products.schema';
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
 	let service: ProductsService;
+	let mongod: MongoMemoryServer;
+	let mongoConnection: Connection;
+	let userModel: Model<User>;
+	let productModel: Model<Product>;
 
-	beforeEach(async () => {
+	beforeAll(async () => {
+		mongod = await MongoMemoryServer.create();
+		const uri = mongod.getUri();
+		mongoConnection = (await connect(uri)).connection;
+		userModel = mongoConnection.model(User.name, UserSchema);
+		productModel = mongoConnection.model(Product.name, ProductSchema);
+
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [ProductsService],
+			providers: [
+				ProductsService,
+				{
+					provide: getModelToken(Product.name),
+					useValue: productModel,
+				},
+				{
+					provide: getModelToken(User.name),
+					useValue: userModel,
+				},
+			],
 		}).compile();
 
 		service = module.get<ProductsService>(ProductsService);
+	});
+
+	afterAll(async () => {
+		await mongoConnection.dropDatabase();
+		await mongoConnection.close();
+		await mongod.stop();
+	});
+
+	afterEach(async () => {
+		const collections = mongoConnection.collections;
+		for (const key in collections) {
+			const collection = collections[key];
+			await collection.deleteMany({});
+		}
 	});
 
 	it('should be defined', () => {
