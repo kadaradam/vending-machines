95cb259795d540ca14d5d302b54f81835ec34396
kadaradam <xadam1996@gmail.com>
2022-09-01 22:26:30 +0200

    feat: users.service.spec: make it work

--- apps/backend/src/users/users.service.spec.ts
+++ apps/backend/src/users/users.service.spec.ts
@@ -1,18 +1,57 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { connect, Connection, Model } from 'mongoose';
+import { Product, ProductSchema } from 'src/products/products.schema';
+import { User, UserSchema } from 'src/users/user.schema';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+	let service: UsersService;
+	let mongod: MongoMemoryServer;
+	let mongoConnection: Connection;
+	let userModel: Model<User>;
+	let productModel: Model<Product>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+	beforeAll(async () => {
+		mongod = await MongoMemoryServer.create();
+		const uri = mongod.getUri();
+		mongoConnection = (await connect(uri)).connection;
+		userModel = mongoConnection.model(User.name, UserSchema);
+		productModel = mongoConnection.model(Product.name, ProductSchema);
 
-    service = module.get<UsersService>(UsersService);
-  });
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				UsersService,
+				{
+					provide: getModelToken(User.name),
+					useValue: userModel,
+				},
+				{
+					provide: getModelToken(Product.name),
+					useValue: productModel,
+				},
+			],
+		}).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+		service = module.get<UsersService>(UsersService);
+	});
+
+	afterAll(async () => {
+		await mongoConnection.dropDatabase();
+		await mongoConnection.close();
+		await mongod.stop();
+	});
+
+	afterEach(async () => {
+		const collections = mongoConnection.collections;
+		for (const key in collections) {
+			const collection = collections[key];
+			await collection.deleteMany({});
+		}
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
