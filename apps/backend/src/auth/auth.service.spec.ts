87735d18a49d3d2bb451256d8ea3b53f29348b23
kadaradam <xadam1996@gmail.com>
2022-09-02 10:19:20 +0200

    feat: add auth.service.spec: login & register test

--- apps/backend/src/auth/auth.service.spec.ts
+++ apps/backend/src/auth/auth.service.spec.ts
@@ -1,13 +1,34 @@
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { connect, Connection, Model } from 'mongoose';
 import { User, UserSchema } from 'src/users/user.schema';
+import { UserExistsException } from 'test/exceptions';
+import { UserDTOStub } from 'test/stubs';
 import { AuthService } from './auth.service';
+
+const JWT_SECRET = 'example';
+
+UserSchema.pre('save', async function (next) {
+	console.log('asd', this);
+	// eslint-disable-next-line @typescript-eslint/no-this-alias
+	const user = this;
+
+	// only hash the password if it has been modified (or is new)
+	if (!user.isModified('password')) return next();
+
+	const hashedPassword = await bcrypt.hash(user.password, 8);
+
+	user.password = hashedPassword;
+
+	next();
+});
 
 describe('AuthService', () => {
 	let service: AuthService;
+	let jwtService: JwtService;
 	let mongod: MongoMemoryServer;
 	let mongoConnection: Connection;
 	let userModel: Model<User>;
@@ -19,8 +40,15 @@ describe('AuthService', () => {
 		userModel = mongoConnection.model(User.name, UserSchema);
 
 		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				JwtModule.register({
+					secretOrPrivateKey: JWT_SECRET,
+					signOptions: {
+						expiresIn: 3600,
+					},
+				}),
+			],
 			providers: [
-				JwtService,
 				AuthService,
 				{
 					provide: getModelToken(User.name),
@@ -30,6 +58,7 @@ describe('AuthService', () => {
 		}).compile();
 
 		service = module.get<AuthService>(AuthService);
+		jwtService = module.get<JwtService>(JwtService);
 	});
 
 	it('should be defined', () => {
@@ -48,5 +77,68 @@ describe('AuthService', () => {
 			const collection = collections[key];
 			await collection.deleteMany({});
 		}
+	});
+
+	describe('userRegister', () => {
+		it('should register a new user', async () => {
+			const reigsterUsername = 'John Doe';
+			const registerPassword = 'changeme';
+			const registeredUser = await service.register({
+				username: reigsterUsername,
+				password: registerPassword,
+			});
+
+			const checkedUser = await userModel.findById(registeredUser._id).select('+password');
+
+			expect(checkedUser.username).toBe(reigsterUsername);
+			expect(await bcrypt.compare(registerPassword, checkedUser.password)).toBe(true);
+		});
+
+		it('should throw an already registered user error', async () => {
+			const withUser = UserDTOStub();
+			await new userModel({
+				...withUser,
+				password: 'changeme',
+			}).save();
+
+			await expect(
+				service.register({
+					username: withUser.username,
+					password: 'changeme',
+				}),
+			).rejects.toThrow(new UserExistsException());
+		});
+	});
+
+	describe('loginUser', () => {
+		it('should log in successfully', async () => {
+			const loginPassword = 'changeme';
+			const withUser = UserDTOStub();
+			await new userModel({
+				...withUser,
+				password: loginPassword,
+			}).save();
+
+			const validatedUser: any = await service.validateUser(withUser.username, loginPassword);
+			expect(validatedUser.password).toBeUndefined();
+			expect(validatedUser.username).toBe(withUser.username);
+
+			const result = await service.login(validatedUser);
+			const decodedJwt: any = jwtService.decode(result.access_token);
+
+			expect(decodedJwt.sub).toBe(withUser._id.toString());
+			expect(decodedJwt.username).toBe(withUser.username);
+		});
+
+		it('should fail to log in: invalid password', async () => {
+			const withUser = UserDTOStub();
+			await new userModel({
+				...withUser,
+				password: 'changeme',
+			}).save();
+
+			const validatedUser: any = await service.validateUser(withUser.username, 'changeme_pw');
+			expect(validatedUser).toBeNull();
+		});
 	});
 });
