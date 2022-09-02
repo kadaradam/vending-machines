019cc6d08971dbbb391704e742dda4555a2c9a1b
kadaradam <xadam1996@gmail.com>
2022-09-02 08:55:13 +0200

    fix: make auth.service.spec.ts run

--- apps/backend/src/auth/auth.service.spec.ts
+++ apps/backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,52 @@
+import { JwtService } from '@nestjs/jwt';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { connect, Connection, Model } from 'mongoose';
+import { User, UserSchema } from 'src/users/user.schema';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+	let service: AuthService;
+	let mongod: MongoMemoryServer;
+	let mongoConnection: Connection;
+	let userModel: Model<User>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+	beforeAll(async () => {
+		mongod = await MongoMemoryServer.create();
+		const uri = mongod.getUri();
+		mongoConnection = (await connect(uri)).connection;
+		userModel = mongoConnection.model(User.name, UserSchema);
 
-    service = module.get<AuthService>(AuthService);
-  });
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				JwtService,
+				AuthService,
+				{
+					provide: getModelToken(User.name),
+					useValue: userModel,
+				},
+			],
+		}).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+		service = module.get<AuthService>(AuthService);
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	afterAll(async () => {
+		await mongoConnection.dropDatabase();
+		await mongoConnection.close();
+		await mongod.stop();
+	});
+
+	afterEach(async () => {
+		const collections = mongoConnection.collections;
+		for (const key in collections) {
+			const collection = collections[key];
+			await collection.deleteMany({});
+		}
+	});
 });
